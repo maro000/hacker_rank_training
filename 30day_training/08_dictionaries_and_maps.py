6b01dff979e05487c2e7ac39465c8776f81a61cc
Nakagawa, Takayoshi <ts-takayosh.nakagawa@rakuten.com>
2020-02-12 16:24:34 +0900

    30days_practice

--- 30day_training/08_dictionaries_and_maps.py
+++ 30day_training/08_dictionaries_and_maps.py
@@ -1,9 +1,33 @@
 n = int(input())
 
-l = []
+l = {}
 for i in range(n):
     a , b =  map(str, input().split())
-    l.append((a,b))
+    name = a
+    phone = int(b) 
+    l[name] = phone
 
-print(l)
-        
+
+for j in range(n):
+  name =input()
+  if name in l:
+    phone = l[name]
+    print(name + '=' + str(phone))
+  else:
+    print('Not found')
+
+
+# 下じゃ無いとなぜか通らない
+
+n = int(input())
+name_numbers = [input().split() for _ in range(n)]
+phone_book = {k: v for k,v in name_numbers}
+while True:
+    try:
+        name = input()
+        if name in phone_book:
+            print('%s=%s' % (name, phone_book[name]))
+        else:
+            print('Not found')
+    except:
+        break
